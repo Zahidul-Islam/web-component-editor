44bd4528c223235543a4d12b73e09773814ea471
Zahidul Islam <zahidsharp@outlook.com>
2016-05-09 10:00:27 +0200

    update components

--- public/js/editor.js
+++ public/js/editor.js
@@ -2,9 +2,12 @@ var editor = ace.edit('editor');
 editor.session.setMode("ace/mode/html");
 editor.setTheme("ace/theme/monokai");
 
-
 var preview = $('#preview');
 
 editor.getSession().on('change', function(){
 	preview.html(editor.getSession().getValue());
+});
+
+$("button").click(function() {
+	$("#editor").toggle();
 });
