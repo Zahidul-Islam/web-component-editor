964a5e76038193647352de26dcd09535cd4619f1
Zahidul Islam <zahidsharp@outlook.com>
2016-05-12 17:11:35 -0700

    Update editor

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,13 +1,44 @@
-var editor = ace.edit('editor');
-editor.session.setMode("ace/mode/html");
-editor.setTheme("ace/theme/monokai");
+const editor = ace.edit('editor');
+const preview = $('#preview');
+var isExternalDSL = true;
 
-var preview = $('#preview');
-
-editor.getSession().on('change', function(){
-	preview.html(editor.getSession().getValue());
+$(() => {
+	editor.session.setMode("ace/mode/jade");
+	editor.setTheme("ace/theme/chrome");	
 });
 
-$("button").click(function() {
-	$("#editor").toggle();
+$(window).load(() => {
+	setCurrentMode("Text");	
 });
+
+editor.getSession().on('change', () => {
+	var html = isExternalDSL ? jade.compile(editor.getSession().getValue()) 
+							 : editor.getSession().getValue();
+	console.log(html());
+	preview.html(html);
+});
+
+$("#internalDSL").click(function() {
+	htmlMode();
+	isExternalDSL = false;
+});
+
+$("#externalDSL").click(function() {
+	jadeMode();
+	isExternalDSL = true;
+});
+
+const jadeMode = () => {
+	editor.session.setMode("ace/mode/jade");
+	editor.setTheme("ace/theme/chrome");
+	emptyEditorText();	
+	setCurrentMode("Text");
+}
+const htmlMode = () => {
+	editor.session.setMode("ace/mode/html");
+	editor.setTheme("ace/theme/monokai");	
+	emptyEditorText();
+	setCurrentMode("HTML");
+}
+const emptyEditorText = () => editor.setValue("");
+const setCurrentMode = (mode) => $("#mode").html("Editor mode: <b>" + mode + "</b>");
