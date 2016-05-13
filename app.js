254ed707ffcb73e54353477094626e96b6b8b6d5
Zahidul Islam <zahidsharp@outlook.com>
2016-05-12 17:15:29 -0700

    update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app     = express();
-var path    = require('path');
+const express = require('express');
+const app     = express();
+const path    = require('path');
 
 app.use('/static', express.static(__dirname + '/public'));
 app.set('view engine', 'html');
@@ -9,5 +9,4 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(3000);
-console.log("Running at port 3000");
+app.listen(3000, () => console.log("Running at port 3000"));
